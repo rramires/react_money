90f4cf0d93a7df8bc14c418738ffc7ca978d250d
rramires <ricardo@flexbr.com>
2025-09-14 23:15:03 -0300

    Adding Summary.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const HeaderContainer = styled.header`
 	background-color: ${(props) => props.theme['gray-900']};
 
-	padding: 1.5rem 0;
+	padding: 1.5rem 0 7.5rem;
 `
 
 export const HeaderContent = styled.div`
