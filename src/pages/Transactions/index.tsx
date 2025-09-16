f164dc1d7b6a5333463bdde7edf3c1ef69683ba6
rramires <ricardo@flexbr.com>
2025-09-15 22:35:20 -0300

    Adding Transactions Table.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+	PriceHighLight,
+	TransactionsContainer,
+	TransactionsTable,
+} from './styles'
 
 export function Transactions() {
 	return (
 		<div>
 			<Header />
 			<Summary />
+
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td>Desenvolvimento de site</td>
+							<td>
+								<PriceHighLight variant='income'>
+									R$ 12.000,00
+								</PriceHighLight>
+							</td>
+							<td>Venda</td>
+							<td>13/04/2025</td>
+						</tr>
+						<tr>
+							<td>Hamburguer</td>
+							<td>
+								<PriceHighLight variant='outcome'>
+									- R$ 59,00
+								</PriceHighLight>
+							</td>
+							<td>Alimentação</td>
+							<td>10/04/2025</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</div>
 	)
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const TransactionsContainer = styled.main`
+	width: 100%;
+	max-width: 70rem;
+	margin: 4rem auto 1rem;
+	padding: 0 1.5rem;
+`
+
+export const TransactionsTable = styled.table`
+	width: 100%;
+
+	border-collapse: separate;
+	border-spacing: 0 0.5rem;
+
+	td {
+		padding: 1.25rem 2rem;
+		background: ${(props) => props.theme['gray-700']};
+
+		&:first-child {
+			width: 50%;
+
+			border-top-left-radius: 6px;
+			border-bottom-left-radius: 6px;
+		}
+
+		&:last-child {
+			border-top-right-radius: 6px;
+			border-bottom-right-radius: 6px;
+		}
+	}
+`
+
+interface PriceHighLightProps {
+	variant: 'income' | 'outcome'
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+	color: ${(props) =>
+		props.variant === 'income'
+			? (props) => props.theme['green-300']
+			: (props) => props.theme['red-300']};
+`
